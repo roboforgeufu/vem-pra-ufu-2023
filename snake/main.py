ba76dc844e8f2960036f86bc211044fb80cd918a
Otávio Leite <atoivat.o@gmail.com>
2023-04-11 20:56:20 -0300

    improvements on snake code

--- snake/main.py
+++ snake/main.py
@@ -4,6 +4,9 @@ from pybricks.ev3devices import Motor, UltrasonicSensor
 from pybricks.hubs import EV3Brick
 from pybricks.parameters import Port, Stop
 
+BITE_DISTANCE = 100
+WATCH_DISTANCE = 500
+
 
 class Snake:
     def __init__(
@@ -19,6 +22,26 @@ class Snake:
         self.body = Motor(body_port)
         self.neck = Motor(neck_port)
 
+    def bite(self):
+        self.body.run(0)
+        self.neck.run(0)
+
+        self.mouth.run_until_stalled(300)
+        self.mouth.run_until_stalled(-300, then=Stop.HOLD)
+
+        self.neck.run(0)
+        self.neck.run(150)
+        self.body.run_time(-300, 1000, then=Stop.BRAKE)
+        self.neck.run(-150)
+        self.body.run_time(-300, 1000, then=Stop.BRAKE)
+        self.neck.run(0)
+
+    def is_watching(self):
+        return self.eyes.distance() < WATCH_DISTANCE
+
+    def is_bite_distance(self):
+        return self.eyes.distance() < BITE_DISTANCE
+
     def cycle(self):
         self.mouth.run_until_stalled(150)
         self.mouth.reset_angle(0)
@@ -30,21 +53,28 @@ class Snake:
         self.neck.run_time(-200, 1000, then=Stop.BRAKE)
         rightmost_neck = self.neck.angle()
 
-        print(leftmost_neck, rightmost_neck)
-
         cycle_neck_direction = "left"
         while True:
+            print(self.eyes.distance())
+            neck_speed = 100
+
+            if self.is_watching():
+                neck_speed = 0
+
+            if self.is_bite_distance():
+                self.bite()
+
             self.body.run(300)
             if cycle_neck_direction == "left":
                 if abs(self.neck.angle() - leftmost_neck) <= 30:
                     cycle_neck_direction = "right"
                 else:
-                    self.neck.run(150)
+                    self.neck.run(neck_speed)
             if cycle_neck_direction == "right":
                 if abs(self.neck.angle() - rightmost_neck) <= 30:
                     cycle_neck_direction = "left"
                 else:
-                    self.neck.run(-150)
+                    self.neck.run(-neck_speed)
 
 
 def main():
